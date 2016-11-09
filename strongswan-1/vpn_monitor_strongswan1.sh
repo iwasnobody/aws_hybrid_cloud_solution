4324166becbacd05b31dc0a38dda2637291676dc
iwasnobody <ivanyu1987@gmail.com>
2016-11-09 17:13:52 +0800

    Update vpn_monitor_strongswan1.sh

--- strongswan-1/vpn_monitor_strongswan1.sh
+++ strongswan-1/vpn_monitor_strongswan1.sh
@@ -4,3 +4,6 @@ litterbin=$(ip add add 169.254.100.2/24 remote 169.254.100.1/24 dev tunnel1)
 litterbin=$(ip link set tunnel1 up mtu 1436)
 
 litterbin=$(ip rule add priority 10 from all lookup 10)
+
+ip route add 169.254.100.0/24 via 169.254.100.1 table 10
+ip route add 10.10.0.0/16 via 169.254.100.1 table 10
